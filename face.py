ee1dd2da7a222e3a4f5e6a854481273769bc74f8
Wen Dong <wen.dong@microfocus.com>
2021-11-28 10:12:44 +0800

    part2

--- face.py
+++ face.py
@@ -7,6 +7,7 @@ from cv2 import CascadeClassifier
 from cv2 import rectangle
 import urllib.request
 import os
+import numpy as np
 
 class Face:
     classifier = None
@@ -14,7 +15,6 @@ class Face:
     def get_face_bboxes(cls, pixels):
         HAAR = "haarcascade_frontalface_default.xml"
         if not os.path.isfile(HAAR):
-            print("downloading...")
             HAAR_URL="https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_default.xml"
             urllib.request.urlretrieve(HAAR_URL, "haarcascade_frontalface_default.xml")
             assert(os.path.isfile(HAAR)), "Failed to download %s" % HAAR_URL
@@ -24,13 +24,18 @@ class Face:
         return bboxes
 
     @classmethod
+    def get_face_bboxes_from_pil_image(cls, pil_image):
+        opencvImage = cv2.cvtColor(np.array(pil_image), cv2.COLOR_RGB2BGR)
+        return cls.get_face_bboxes(opencvImage)
+    @classmethod
     def get_face_bboxes_from_file(cls, image_file):
         pixels = imread(image_file)
         return cls.get_face_bboxes(pixels)
 
 def main():
-    test_file = "images/test2.jpg"
+    test_file = "images/278.jpg"
     pixels = imread(test_file)
+    # print(pixels)
     bboxes = Face.get_face_bboxes(pixels)
     print(bboxes)
     for box in bboxes:
@@ -39,7 +44,7 @@ def main():
         rectangle(pixels, (x, y), (x2, y2), (0, 0, 255), 1)
 
     # show the image
-    imshow('faces', pixels)
+    imshow('faces detection', pixels)
     # keep the window open until we press a key
     waitKey(0)
     # close the window
